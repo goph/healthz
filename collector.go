fd1da7e481e50c11556eba667fe57526d2a75155
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-05-31 00:30:37 +0200

    Add collector, remove liveness and readiness

---
+++ collector.go
@@ -0,0 +1,21 @@
+package healthz
+
+// Collector is a global context structure to accept checkers from all kinds of sources.
+// It aggregates them and returns a HealthService.
+type Collector map[string][]Checker
+
+// RegisterChecker registers a new checker for a specific check.
+func (c Collector) RegisterChecker(check string, checker Checker) {
+	c[check] = append(c[check], checker)
+}
+
+// NewHealthService aggregates the checkers and returns a new HealthService.
+func (c Collector) NewHealthService() HealthService {
+	healthService := make(HealthService)
+
+	for t, checkers := range c {
+		healthService[t] = NewCheckers(checkers...)
+	}
+
+	return healthService
+}
--- healthz.go
+++ healthz.go
@@ -1,15 +1,31 @@
 package healthz
 
-// HealthService is a helper struct which wraps two Checkers and exposes them as HTTP handlers.
-type HealthService struct {
-	livenessChecker  Checker
-	readinessChecker Checker
-}
+import "net/http"
 
-// NewHealthService creates a new HealthService from user configured Checkers.
-func NewHealthService(livenessChecker Checker, readinessChecker Checker) *HealthService {
-	return &HealthService{
-		livenessChecker:  livenessChecker,
-		readinessChecker: readinessChecker,
+// HealthService wraps Checkers and exposes them as HTTP handlers.
+type HealthService map[string]Checker
+
+// Handler returns an http.Handler for a check.
+// If a check is not found the returned handler will always return success.
+func (s HealthService) Handler(check string) http.Handler {
+	checker, ok := s[check]
+	if !ok {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("ok"))
+		})
 	}
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// If the checker fails, we return an error
+		if err := checker.Check(); err != nil {
+			w.WriteHeader(http.StatusServiceUnavailable)
+			w.Write([]byte("error"))
+
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
+	})
 }
